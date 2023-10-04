8c0be1ce9dd37154ee163c59ccf0204a8afbcb44
JGraham-appdev <jgraham@appdev.yearup.org>
2023-10-04 10:06:46 -0400

    completed ShoppingList app

---
+++ workbook-1/ShoppingLIst/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("ham");
+        System.out.println("bun");
+        System.out.println("Lollipop");
+        System.out.println("bread");
+        System.out.println("socks");
+        System.out.println("chicharrones");
+        System.out.println("almonds");
+        System.out.println("uno cards");
+        System.out.println("Soda");
+        System.out.println("piggy bank");
+    }
+}
